b549c2c853ce051a46cd06750b2f2f496a62133d
Maria dels Àngels Cerveró Abelló <r5oc9mo2f06qk5rdo29cemjs4rnvu0@bots.bitbucket.org>
2023-12-06 13:29:29 +0100

    AOC Day 2 - Problem 2 solved

--- 2023_problems/day02.cpp
+++ 2023_problems/day02.cpp
@@ -6,6 +6,7 @@
 int problem01Solver(std::string filename);
 int problem02Solver(std::string filename);
 bool areColorCubesValid(std::string line, std::regex regex, const int COLOR_CUBES);
+int getMinValidColorCubes(std::string line, std::regex regex);
 
 const int RED_CUBES = 12;
 const int GREEN_CUBES = 13;
@@ -62,7 +63,25 @@ int problem01Solver(std::string filename) {
 }
 
 int problem02Solver(std::string filename) {
-    return 0;
+    std::string line;
+    std::ifstream ifile(filename);
+    std::regex redRegex("(\\d*) red");
+    std::regex greenRegex("(\\d*) green");
+    std::regex blueRegex("(\\d*) blue");
+    int minRed, minGreen, minBlue, sum = 0;
+
+    if(!ifile.is_open()) return -1;
+    else {
+        while(std::getline(ifile, line)) {
+            minRed = getMinValidColorCubes(line, redRegex);
+            minGreen = getMinValidColorCubes(line, greenRegex);
+            minBlue = getMinValidColorCubes(line, blueRegex);
+
+            sum += minRed * minGreen * minBlue;
+        }
+    }
+    
+    return sum;
 }
 
 bool areColorCubesValid(std::string line, std::regex regex, const int COLOR_CUBES) {
@@ -81,4 +100,23 @@ bool areColorCubesValid(std::string line, std::regex regex, const int COLOR_CUBE
     }
 
     return isValid;
+}
+
+int getMinValidColorCubes(std::string line, std::regex regex) {
+    std::smatch colorMatch;
+    bool matched;
+    int ncubes, minCubes = 0;
+
+    matched = std::regex_search(line, colorMatch, regex);
+    while(matched) {
+        ncubes = std::stoi(std::string(colorMatch[1]));
+        if(ncubes > minCubes) {
+            minCubes = ncubes; 
+        }
+
+        line = std::string(colorMatch.suffix());
+        matched = std::regex_search(line, colorMatch, regex);
+    }
+
+    return minCubes;
 }
