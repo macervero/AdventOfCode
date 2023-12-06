a400ae431d673b7b1b8306bc9048be4004918ddc
Maria dels Àngels Cerveró Abelló <r5oc9mo2f06qk5rdo29cemjs4rnvu0@bots.bitbucket.org>
2023-12-06 13:18:44 +0100

    AOC Day 2 - Problem 1 solved

---
+++ 2023_problems/day02.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+int problem01Solver(std::string filename);
+int problem02Solver(std::string filename);
+bool areColorCubesValid(std::string line, std::regex regex, const int COLOR_CUBES);
+
+const int RED_CUBES = 12;
+const int GREEN_CUBES = 13;
+const int BLUE_CUBES = 14;
+
+int main(int argc, char** argv) {
+    std::string filename;
+    bool executeBoth = false;
+    int sum;
+
+    if(argc == 1) executeBoth = true;
+
+    if(executeBoth || *argv[1] == '1') {
+        filename = "input1_day02.txt";
+        sum = problem01Solver(filename);
+        if(sum == -1) std::cerr << filename << " can not be opened" << std::endl;
+        else std::cout << sum << std::endl;
+    }
+
+    if(executeBoth || *argv[1] == '2') {
+        filename = "input2_day02.txt";
+        sum = problem02Solver(filename);
+        if(sum == -1) std::cerr << filename << " can not be opened" << std::endl;
+        else std::cout << sum << std::endl;
+    }
+
+    return 0;
+}
+
+int problem01Solver(std::string filename) {
+    std::string line;
+    std::ifstream ifile(filename);
+    std::regex idRegex("Game (\\d*):");
+    std::regex redRegex("(\\d*) red");
+    std::regex greenRegex("(\\d*) green");
+    std::regex blueRegex("(\\d*) blue");
+    std::smatch idMatch;
+    int id, sum = 0;
+
+    if(!ifile.is_open()) return -1;
+    else {
+        while(std::getline(ifile, line)) {
+            std::regex_search(line, idMatch, idRegex);
+            id = std::stoi(std::string(idMatch[1]));
+
+            if(areColorCubesValid(line, redRegex, RED_CUBES) &&
+                areColorCubesValid(line, greenRegex, GREEN_CUBES) &&
+                areColorCubesValid(line, blueRegex, BLUE_CUBES)) {
+                sum += id;
+            }
+        }
+    }
+    return sum;
+}
+
+int problem02Solver(std::string filename) {
+    return 0;
+}
+
+bool areColorCubesValid(std::string line, std::regex regex, const int COLOR_CUBES) {
+    std::smatch colorMatch;
+    bool matched, isValid = true;
+
+    matched = std::regex_search(line, colorMatch, regex);
+    while(matched) {
+        if(std::stoi(std::string(colorMatch[1])) > COLOR_CUBES) {
+            isValid = false;
+            matched = false;
+        } else {
+            line = std::string(colorMatch.suffix());
+            matched = std::regex_search(line, colorMatch, regex);
+        }
+    }
+
+    return isValid;
+}
